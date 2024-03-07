aeb38aaae9ce7a3f08541bcf5366109979838eec
Guilherme Halter Nunes <halter.guilhermenunes@gmail.com>
2024-03-06 21:25:19 -0300

    Feito atividade variaveis

---
+++ ex003/index.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+	
+	int idade;
+	idade = 18;
+	
+	char primeiraLetra;
+	primeiraLetra = 'G';
+	
+	float altura;
+	altura = 1.86;
+	
+	printf("Letra = %c\n",primeiraLetra);
+	printf("Idade = %i\n",idade);
+	printf("Altura = %.2f\n",altura);
+	
+	return 0 ;
+}

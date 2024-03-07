0a42fa83dd6fe3d4a3e2a17198aa9f54c278915e
Guilherme Halter Nunes <halter.guilhermenunes@gmail.com>
2024-03-06 21:08:22 -0300

    Feito exercicio de dados
    	new file:   ex002/index.exe

---
+++ ex002/index.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+	
+	
+	printf("Nome: Guilherme Halter Nunes \n");
+	printf("Endereco: Rua Maria josé de Souza Oliveira \n");
+	printf("Telefone: (55) 47 99658-5328 \n");
+	printf("E-mail: halter.guilhermenunes@gmail.com \n");
+	printf("CPF: 139.930.609-03 \n");
+	
+	return 0;
+}
